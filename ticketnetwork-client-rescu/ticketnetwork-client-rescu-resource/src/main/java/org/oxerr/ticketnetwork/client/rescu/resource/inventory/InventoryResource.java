f7126751e776785ce4c6c1f5205afa70c1b31945
Joe Zhou <zhoushuqun@gmail.com>
2025-02-25 02:00:46 +0800

    Add patch ticket group.

--- ticketnetwork-client-rescu/ticketnetwork-client-rescu-resource/src/main/java/org/oxerr/ticketnetwork/client/rescu/resource/inventory/InventoryResource.java
+++ ticketnetwork-client-rescu/ticketnetwork-client-rescu-resource/src/main/java/org/oxerr/ticketnetwork/client/rescu/resource/inventory/InventoryResource.java
@@ -7,9 +7,12 @@ import org.oxerr.ticketnetwork.client.model.TicketGroupV4PostModel;
 import org.oxerr.ticketnetwork.client.model.TicketGroupsV4GetModel;
 import org.oxerr.ticketnetwork.client.rescu.resource.TicketNetworkException;
 
+import com.github.fge.jsonpatch.JsonPatchOperation;
+
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -21,12 +24,13 @@ import jakarta.ws.rs.core.MediaType;
  * A RESTful API that lets subscribers interact with tickets for brokers.
  */
 @Path("/inventory/v5")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public interface InventoryResource {
 
 	/**
-	 * Search for ticket groups by filter. Multiple parameters are treated as
+	 * Search for ticket groups by filter.
+	 * 
+	 * <p>
+	 * Multiple parameters are treated as
 	 * AND. For ticket-level properties, if any ticket in the ticket group
 	 * matches, the entire ticket group is included. Refer to the OData standard
 	 * for more information on [$filter]
@@ -38,6 +42,7 @@ public interface InventoryResource {
 	 * '[contains](http://docs.oasis-open.org/odata/odata/v4.0/errata02/os/complete/part2-url-conventions/odata-v4.0-errata02-os-part2-url-conventions-complete.html#_Toc406398117)',
 	 * '[startswith](http://docs.oasis-open.org/odata/odata/v4.0/errata02/os/complete/part2-url-conventions/odata-v4.0-errata02-os-part2-url-conventions-complete.html#_Toc406398119)'.
 	 * The syntax for 'in' is in(path,[val1,val2,...]).
+	 * </p>
 	 *
 	 * <p>
 	 * If successful, the HTTP response code will indicate a 200 (OK) response,
@@ -101,6 +106,8 @@ public interface InventoryResource {
 	 */
 	@GET
 	@Path("/ticketgroups")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
 	TicketGroupsV4GetModel getTicketGroups(
 		@QueryParam("hasEticket") Boolean hasEticket,
 		@QueryParam("pending") Boolean pending,
@@ -115,9 +122,11 @@ public interface InventoryResource {
 	/**
 	 * Create a ticket group.
 	 *
+	 * <p>
 	 * If successful, the HTTP response code will indicate a 201 (Created)
 	 * response, and a representation of the ticket group will be included in
 	 * the response.
+	 * </p>
 	 *
 	 * @param ticketGroupV4PostModel Object containing details of the ticket
 	 * group to create.
@@ -127,11 +136,19 @@ public interface InventoryResource {
 	 */
 	@POST
 	@Path("/ticketgroups")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
 	TicketGroupV4GetModel createTicketGroup(TicketGroupV4PostModel ticketGroupV4PostModel)
 		throws IOException, TicketNetworkException;
 
 	/**
 	 * Deletes a single ticket group by ID.
+	 *
+	 * <p>
+	 * If successful, the HTTP response code will indicate a 204 (No Content)
+	 * response. If there is not currently a QR screenshot attached, the call
+	 * will fail.
+	 * </p>
 	 *
 	 * @param ticketGroupId The ticket group ID.
 	 * @throws IOException if an I/O error occurs.
@@ -139,7 +156,89 @@ public interface InventoryResource {
 	 */
 	@DELETE
 	@Path("/ticketgroups/{ticketGroupId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
 	void deleteTicketGroup(@PathParam("ticketGroupId") Integer ticketGroupId)
 		throws IOException, TicketNetworkException;
+
+	/**
+	 * Updates an ticket group according to a list of PATCH operations.
+	 *
+	 * <p>
+	 * Include the following media type in the request HTTP Headers:
+	 * {@code Content-Type: application/json-patch+json}.
+	 * For the full IETF specification on PATCH syntax see
+	 * <a href="http://tools.ietf.org/html/rfc6902">JavaScript Object Notation (JSON) Patch – RFC6902</a>.
+	 * </p>
+	 * 
+	 * <p>
+	 * If successful, the HTTP response code will indicate a 200 (OK) response,
+	 * and a representation of the newly modified order (after the PATCH
+	 * operations have been completed) will be included in the response.
+	 * On failure, the API will respond with a 400 (Bad Request) with
+	 * appropriate validation message(s).
+	 * </p>
+	 *
+	 * <h3>Patchable Properties</h3>
+	 * <p>
+	 * <b>Add</b>:
+	 * event/id, seats/section, seats/row, seats/standardSection,
+	 * seats/canonicalSection, seats/seatingType/id, seats/lowSeat,
+	 * unitPrice/wholesalePrice/value, unitPrice/wholesalePrice/currencyCode,
+	 * unitPrice/retailPrice/value, unitPrice/retailPrice/currencyCode,
+	 * unitPrice/facePrice/currencyCode, unitPrice/facePrice/value,
+	 * unitPrice/cost/value, unitPrice/cost/currencyCode,
+	 * unitPrice/taxedCost/value, unitPrice/taxedCost/currencyCode,
+	 * unitPrice/msrp/value, unitPrice/msrp/currencyCode, isOnHand, onHandDate,
+	 * referenceTicketGroupId, stockType/id, ticketGroupType/id, splitRule/id,
+	 * broadcastChannelIds, notes/external, notes/internal, notes/additional,
+	 * nearTerm/nearTermDeliveryMethod/id, nearTerm/nearTermDisplay/id, pending,
+	 * tags, autopricer/endPrice, autopricer/percentOfWholesale,
+	 * autopricer/startDaysBeforeEvent, autopricer/endDaysBeforeEvent.
+	 * </p>
+	 *
+	 * <p>
+	 * <b>Replace</b>:
+	 * event/id, seats/section, seats/row, seats/standardSection,
+	 * seats/canonicalSection, seats/seatingType/id, seats/lowSeat,
+	 * unitPrice/wholesalePrice/value, unitPrice/wholesalePrice/currencyCode,
+	 * unitPrice/retailPrice/value, unitPrice/retailPrice/currencyCode,
+	 * unitPrice/facePrice/currencyCode, unitPrice/facePrice/value,
+	 * unitPrice/cost/value, unitPrice/cost/currencyCode,
+	 * unitPrice/taxedCost/value, unitPrice/taxedCost/currencyCode,
+	 * unitPrice/msrp/value, unitPrice/msrp/currencyCode, isOnHand, onHandDate,
+	 * referenceTicketGroupId, stockType/id, ticketGroupType/id, splitRule/id,
+	 * quantity/total, broadcastChannelIds, notes/external, notes/internal,
+	 * notes/additional, nearTerm/nearTermDeliveryMethod/id,
+	 * nearTerm/nearTermDisplay/id, hideSeats, pending, isShort, tags,
+	 * autopricer/isActive, autopricer/endPrice, autopricer/percentOfWholesale,
+	 * autopricer/startDaysBeforeEvent, autopricer/endDaysBeforeEvent.
+	 * </p>
+	 *
+	 * <p>
+	 * <b>Remove</b>:
+	 * referenceTicketGroupId, broadcastChannelIds, notes/external,
+	 * notes/internal, notes/additional, tags.
+	 * </p>
+	 *
+	 * @param ticketGroupId The ticket group ID.
+	 * @param patchOperations A list of operations to perform on the ticket
+	 * group in the order in which they should be applied.
+	 *
+	 * @return OK
+	 * @throws IOException if an I/O error occurs.
+	 * @throws TicketNetworkException if a business exception occurs.
+	 * The model provided in the body is invalid, one or more of the operations
+	 * is not supported, or performing one or more of the operations would put
+	 * the ticket groups in an invalid state.
+	 */
+	@PATCH
+	@Path("/ticketgroups/{ticketGroupId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON_PATCH_JSON)
+	TicketGroupV4GetModel updateTicketGroup(
+		@PathParam("ticketGroupId") Integer ticketGroupId,
+		JsonPatchOperation... patchOperations
+	) throws IOException, TicketNetworkException;
 
 }
