825c5566e20c2ef673c3503f3e17958b20de8fcf
Joe Zhou <zhoushuqun@gmail.com>
2025-03-27 22:13:03 +0800

    Add more fields to identify one listing.

--- ticketnetwork-client-cached/ticketnetwork-client-cached-redisson/src/main/java/org/oxerr/ticketnetwork/client/cached/redisson/inventory/ListingInfo.java
+++ ticketnetwork-client-cached/ticketnetwork-client-cached-redisson/src/main/java/org/oxerr/ticketnetwork/client/cached/redisson/inventory/ListingInfo.java
@@ -11,14 +11,26 @@ class ListingInfo {
 
 	private final Integer referenceTicketGroupId;
 
+	private String section;
+
+	private String row;
+
+	private String lowSeat;
+
 	public ListingInfo(TicketNetworkCachedListing c) {
 		this.marketplaceEventId = c.getEvent().getMarketplaceEventId();
 		this.referenceTicketGroupId = c.getRequest().getReferenceTicketGroupId();
+		this.section = c.getRequest().getSection();
+		this.row = c.getRequest().getRow();
+		this.lowSeat = c.getRequest().getLowSeat();
 	}
 
 	public ListingInfo(TicketGroup listing) {
 		this.marketplaceEventId = listing.getEvent().getId();
 		this.referenceTicketGroupId = listing.getReferenceTicketGroupId();
+		this.section = listing.getSeats().getSection();
+		this.row = listing.getSeats().getRow();
+		this.lowSeat = listing.getSeats().getLowSeat().toString();
 	}
 
 	public Integer getMarketplaceEventId() {
@@ -27,6 +39,18 @@ class ListingInfo {
 
 	public Integer getReferenceTicketGroupId() {
 		return referenceTicketGroupId;
+	}
+
+	public String getSection() {
+		return section;
+	}
+
+	public String getRow() {
+		return row;
+	}
+
+	public String getLowSeat() {
+		return lowSeat;
 	}
 
 	@Override
