a0646700029adc3ba006eea55229fad6687dfd72
Joe Zhou <zhoushuqun@gmail.com>
2025-04-06 19:20:19 +0800

    Fix the scope equals.

--- ticketnetwork-client-rescu/ticketnetwork-client-rescu-impl/src/test/java/org/oxerr/ticketnetwork/client/rescu/impl/oauth2/TokenServiceImplTest.java
+++ ticketnetwork-client-rescu/ticketnetwork-client-rescu-impl/src/test/java/org/oxerr/ticketnetwork/client/rescu/impl/oauth2/TokenServiceImplTest.java
@@ -3,17 +3,20 @@ package org.oxerr.ticketnetwork.client.rescu.impl.oauth2;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.oxerr.ticketnetwork.client.rescu.impl.ResCUTicketNetworkClients;
-
-@Disabled("This test requires the access_token.")
 class TokenServiceImplTest {
 
 	private final Logger log = LogManager.getLogger();
 
+	@Disabled("This test requires the access_token.")
 	@Test
 	void testGenerateToken() {
 		var grantType = "client_credentials";
@@ -21,7 +24,12 @@ class TokenServiceImplTest {
 		var scope = "sections_get standard_section_scope exchange_inventory_read batch_pricing_write";
 		var token = ResCUTicketNetworkClients.createKeyManagerClient().getTokenService().generateToken(grantType, scope);
 		log.info("Token: {}", token);
-		assertEquals(scope, token.getScope());
+
+		Set<String> expectedScopeSet = Arrays.stream(scope.split(" ")).collect(Collectors.toSet());
+		Set<String> actualScopeSet = Arrays.stream(token.getScope().split(" ")).collect(Collectors.toSet());
+
+		assertEquals(expectedScopeSet, actualScopeSet);
+
 		assertEquals("Bearer", token.getTokenType());
 		assertNotNull(token.getAccessToken());
 	}
